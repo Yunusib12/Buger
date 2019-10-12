7c7e58f50ff22cb5cd65fd35177db8e96855500d
Ibrahim Yunus <younis.ib@gmail.com>
2019-10-12 01:09:22 -0400

    production config added

--- config/config.js
+++ config/config.js
@@ -17,10 +17,10 @@ config = {
         "operatorsAliases": false
     },
     "production": {
-        "username": process.env.DB_USERNAME,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOSTNAME,
+        "username": process.env.DB_USERNAMEP,
+        "password": process.env.DB_PASSWORDP,
+        "database": process.env.DB_NAMEP,
+        "host": process.env.DB_HOSTNAMEP,
         "dialect": "mysql",
         "operatorsAliases": false
     }
